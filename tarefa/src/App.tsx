bd01099401fceb990779e58fd29c60a98e7aa8d5
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2024-10-08 22:23:41 -0300

    Add files via upload

--- tarefa/src/App.tsx
+++ tarefa/src/App.tsx
@@ -8,10 +8,10 @@ function App() {
   const [corB, setCorB] = useState(0);
 
   return (
-    <Principal>
-      <Input label="R" />
-      <Input label="G" />
-      <Input label="B" />
+    <Principal style={{ backgroundColor: `rgb(${corR}, ${corG}, ${corB})` }}>
+      <Input label="R" value={corR} onChange={(value:number) => setCorR(value)}/>
+      <Input label="G" value={corG} onChange={(value:number) => setCorG(value)}/>
+      <Input label="B" value={corB} onChange={(value:number) => setCorB(value)}/>
     </Principal> 
   );
 }
@@ -19,5 +19,14 @@ function App() {
 export default App;
 
 const Principal = styled.div`
-  background-color: rgb(0,0,0);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  border-radius: 8px;
+  min-height: 150px;
+  width: 555px;
+  margin-top: 17%;
+  margin-left: 34%;
 `;
--- tarefa/src/components/Input.tsx
+++ tarefa/src/components/Input.tsx
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 export default function Input(props:any) {
+    const handler = (e:any) => {
+        const valor = Math.max(0, Math.min(255, e.target.value));
+        props.onChange(valor);
+    }
+
     return(
-        <div>
+        <InputDiv>
             <label>{props.label}</label>
-            <input type="number" />
-        </div>
+            <input type="number" value={props.value} onChange={handler}/>
+        </InputDiv>
     );
 };
+
+const InputDiv = styled.div`
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center; 
+  color: white;
+   margin: 0 10px;
+`
