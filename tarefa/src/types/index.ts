c0ffa1cf13fe7fd185a5cc9660cfbf73af8ae10b
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2024-11-06 20:15:43 -0300

    Delete tarefa directory

--- tarefa/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface RGBContextProps {
-    corR: number;
-    corG: number;
-    corB: number;
-    setRed: (value:string) => void;
-    setGreen: (value:string) => void;
-    setBlue: (value:string) => void;
-    saveRGB: () => void;
-}
