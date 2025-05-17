ea9f7611b6ef6762a6efa535819d8b802992a48e
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2025-05-16 23:49:12 -0300

    Delete listaCompras directory

--- listaCompras/backend/models/lista.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const listaSchema = new mongoose.Schema({
-    nome: { type: String },
-    preco: { type: String }
-})
-
-const Lista = mongoose.model("shoppingitems", listaSchema);
-export default Lista;
